15c183ed84df69148a8c8baa29549da6814ec745
jamesrford7 <jamesrford7@gmail.com>
2019-08-07 17:46:24 +0100

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,2 @@
-// const withSass = require("@zeit/next-sass");
-// const withCSS = require("@zeit/next-css");
-
-// module.exports = withSass({
-//   cssModules: true
-// });
-
-// module.exports = withCSS({
-//   cssModules: true
-// });
-
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS({
-  /* my next config */
-});
+module.exports = withCSS();
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import Nav from "../components/nav";
 
 const Home = () => {
   return (
