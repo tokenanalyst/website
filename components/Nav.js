30882fc0f6dd25dc2140bf74dbd15005058b254b
ogbk <ogbu.k.olu@gmail.com>
2019-08-09 12:36:59 +0100

    style dropdown caret

--- components/Nav.js
+++ components/Nav.js
@@ -226,6 +226,18 @@ const Nav = () => {
           position: relative;
         }
 
+        .link-about::after,
+        .link-contact::after {
+          display: inline-block;
+          margin-left: .255em;
+          vertical-align: .255em;
+          content: "";
+          border-top: .3em solid;
+          border-right: .3em solid transparent;
+          border-bottom: 0;
+          border-left: .3em solid transparent;
+        }
+
         /* ============  SUBLINKS (DESKTOP) ============ */
 
         .navitem {
