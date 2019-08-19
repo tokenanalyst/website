09fdb4356e9c0c4bf8166f2ba3ef49de32de6a9e
jamesrford7 <jamesrford7@gmail.com>
2019-08-19 12:19:51 +0100

    Remove consoles

--- api/exchange-io.js
+++ api/exchange-io.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 module.exports = async (_, res) => {
-  console.log(process.env);
-  console.log("fewf");
   const { data } = await axios.get(
     "https://api.tokenanalyst.io/analytics/last?job=exchange_flows_all_tokens_v5&format=json"
   );
