e568ffd3e7e1e5b6b60e80fa939e73948286efee
David Fava <dfava80@gmail.com>
2019-09-03 16:01:30 +0100

    Add toSingleValueChartData test

--- data-transformers/charts/mappers/toSingleValueChartData.test.js
+++ data-transformers/charts/mappers/toSingleValueChartData.test.js
@@ -1,7 +1,17 @@
 import { toSingleValueChartData } from "./toSingleValueChartData";
 
-describe("Run test suite", () => {
-  it("run a test success", () => {
-    console.log("test running");
+describe("toSingleValueChartData function", () => {
+  it("maps data success", () => {
+    const mockData = [
+      { timeTestKey: "1", valueTestKey: "2" },
+      { timeTestKey: "3", valueTestKey: "4" }
+    ];
+    const expectedData = [{ time: "1", value: "2" }, { time: "3", value: "4" }];
+    const mappedData = toSingleValueChartData(
+      mockData,
+      "timeTestKey",
+      "valueTestKey"
+    );
+    expect(mappedData).toEqual(expectedData);
   });
 });
