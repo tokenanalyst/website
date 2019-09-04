44fc40fe490f73dd5d513b95980b0832a73b7688
David Fava <dfava80@gmail.com>
2019-09-04 12:09:23 +0100

    Fix PR #37

--- components/PricingLink.js
+++ components/PricingLink.js
@@ -1,42 +1,52 @@
 import React from "react";
 import Link from "next/link";
 
-const renderResponsiveText = () => {
-  if (typeof window !== "undefined") {
-    return window.innerWidth > 768 ? "Access historical data" : "Historical data";
-  }
-};
+import { colors } from "../constants/styles/colors";
 
 export const PricingLink = () => {
   return (
     <>
-      <div>
+      <div className="desktop">
         <Link href="/pricing">
-          <a>
-            {renderResponsiveText()}
-          </a>
+          <a>Access historical data</a>
+        </Link>
+      </div>
+      <div className="mobile">
+        <Link href="/pricing">
+          <a>Historical data</a>
         </Link>
       </div>
       <style jsx>{`
+        .mobile {
+          display: none;
+        }
         a {
-          color: rgb(250, 78, 150);
+          color: rgba(${colors.primaryRed}, 1);
           font-weight: 700;
           font-family: Space Grotesk;
           font-size: 18px;
           text-decoration: none;
           border-bottom-style: solid;
           border-bottom-width: 2px;
-          border-bottom-color: rgb(250, 78, 150);
+          border-bottom-color: rgba(${colors.primaryRed}, 1);
         }
         a:visited {
-          color: rgb(250, 78, 150);
+          color: rgba(${colors.primaryRed}, 1);
           font-weight: 700;
           text-decoration: none;
         }
         a:hover {
-          color: rgb(250, 78, 150);
+          color: rgba(${colors.primaryRed}, 1);
           font-weight: 700;
           text-decoration: none;
+        }
+        @media only screen and (max-width: 768px) {
+          .desktop {
+            display: none;
+          }
+          .mobile {
+            display: inline-block;
+          }
         }
       `}</style>
     </>
