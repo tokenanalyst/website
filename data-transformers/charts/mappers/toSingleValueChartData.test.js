a1f06a0fa652591b963273b01e11f80991912dd0
David Fava <dfava80@gmail.com>
2019-09-10 12:46:15 +0100

    Fix tests

--- data-transformers/charts/mappers/toSingleValueChartData.test.js
+++ data-transformers/charts/mappers/toSingleValueChartData.test.js
@@ -1,7 +1,7 @@
 import { toSingleValueChartData } from "./toSingleValueChartData";
 
 describe("toSingleValueChartData function", () => {
-  it("maps data success", () => {
+  it("should map data", () => {
     const mockData = [
       { timeTestKey: "1", valueTestKey: "2" },
       { timeTestKey: "3", valueTestKey: "4" }
