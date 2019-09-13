6d0d9d8823b4733a73a9a5520f9a5538347516a5
jamesrford7 <jamesrford7@gmail.com>
2019-09-13 09:55:23 +0100

    Only enable in prod

--- components/Analytics.js
+++ components/Analytics.js
@@ -5,18 +5,20 @@ import * as Sentry from "@sentry/browser";
 
 export const Analytics = ({ children }) => {
   useEffect(() => {
-    ReactGA.initialize("UA-113322596-1");
-    const handleRouteChange = () => {
-      ReactGA.pageview(Router.asPath);
-    };
-    Router.events.on("routeChangeComplete", handleRouteChange);
-    Sentry.init({
-      dsn: "https://04801cfba732405cae39800a34b707ca@sentry.io/1553818"
-    });
+    if (process.env.NODE_ENV !== "development") {
+      ReactGA.initialize("UA-113322596-1");
+      const handleRouteChange = () => {
+        ReactGA.pageview(Router.asPath);
+      };
+      Router.events.on("routeChangeComplete", handleRouteChange);
+      Sentry.init({
+        dsn: "https://04801cfba732405cae39800a34b707ca@sentry.io/1553818"
+      });
 
-    return () => {
-      Router.events.off("routeChangeComplete", handleRouteChange);
-    };
+      return () => {
+        Router.events.off("routeChangeComplete", handleRouteChange);
+      };
+    }
   }, []);
 
   return <div>{children}</div>;
