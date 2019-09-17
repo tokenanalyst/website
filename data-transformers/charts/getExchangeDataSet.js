691cb6252883e7262dafb9458ee63d75f53ac98e
jamesrford7 <jamesrford7@gmail.com>
2019-09-17 10:47:37 +0100

    Fix up transforms to be a bit more readable

--- data-transformers/charts/getExchangeDataSet.js
+++ data-transformers/charts/getExchangeDataSet.js
@@ -2,174 +2,178 @@ import { toSingleValueChartData } from "./mappers";
 
 import { NATIVE_TOKENS, CURRENCIES } from "../../constants/tokens";
 import { CHART_TYPES } from "../../constants/chartTypes";
-import { getUTCDate } from "./mappers/utils";
 
 const formatTokenSymbol = rawSymbol => rawSymbol.replace("_", " ");
-
-const addTimeWindow = (baseDataSet, timeWindow) => {
-  return baseDataSet.map(dataPoint => {
-    return { ...dataPoint, timeWindow };
-  });
-};
 
 export const getExchangeDataSet = (response, token, timeWindow = "1d") => {
   const USDSymbol = formatTokenSymbol(CURRENCIES.USD);
   const tokenSymbol = formatTokenSymbol(token);
 
-  const toSingleValueChartDataForTimeWindow = (data, timeKey, valueKey) => {
-    if (timeWindow === "1h") {
-      return data.map(datum => ({
-        time: new Date(getUTCDate(datum)).getTime() / 1000,
-        value: datum[valueKey]
-      }));
-    }
-
-    return toSingleValueChartData(data, timeKey, valueKey);
-  };
-
   const baseDataSet = [
     {
       dataPoint: "Price",
       title: "Price",
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.price,
         "date",
-        "price_usd"
+        "price_usd",
+        timeWindow
       ),
       visible: true,
       solidColor: "#0198E1",
       chartType: CHART_TYPES.line,
-      isAlwaysDisplayed: true
+      isAlwaysDisplayed: true,
+      timeWindow
     },
     {
       dataPoint: `Volume (${tokenSymbol})`,
       title: `Outflow Volume (${tokenSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.outflow,
         "date",
-        "outflow"
+        "outflow",
+        timeWindow
       ),
       visible: true,
       solidColor: "rgba(250, 78, 150, 1)",
       topColor: "rgba(250, 78, 150, 0.3)",
-      bottomColor: "rgba(250, 78, 150, 0.04)"
+      bottomColor: "rgba(250, 78, 150, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Volume (${tokenSymbol})`,
       title: `Inflow Volume (${tokenSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.inflow,
         "date",
-        "inflow"
+        "inflow",
+        timeWindow
       ),
       visible: true,
       solidColor: "rgba(63, 205, 171, 1)",
       topColor: "rgba(63, 205, 171, 0.7)",
-      bottomColor: "rgba(63, 205, 171, 0.04)"
+      bottomColor: "rgba(63, 205, 171, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Volume (${USDSymbol})`,
       title: `Outflow Volume (${USDSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.outflow,
         "date",
-        "outflow_usd"
+        "outflow_usd",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(250, 78, 150, 1)",
       topColor: "rgba(250, 78, 150, 0.3)",
-      bottomColor: "rgba(250, 78, 150, 0.04)"
+      bottomColor: "rgba(250, 78, 150, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Volume (${USDSymbol})`,
       title: `Inflow Volume (${USDSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.inflow,
         "date",
-        "inflow_usd"
+        "inflow_usd",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(63, 205, 171, 1)",
       topColor: "rgba(63, 205, 171, 0.7)",
-      bottomColor: "rgba(63, 205, 171, 0.04)"
+      bottomColor: "rgba(63, 205, 171, 0.04)",
+      timeWindow
     },
     {
       dataPoint: "TXN Count",
       title: "Outflow TXN Count",
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.outflow,
         "date",
-        "number_of_txns"
+        "number_of_txns",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(250, 78, 150, 1)",
       topColor: "rgba(250, 78, 150, 0.3)",
-      bottomColor: "rgba(250, 78, 150, 0.04)"
+      bottomColor: "rgba(250, 78, 150, 0.04)",
+      timeWindow
     },
     {
       dataPoint: "TXN Count",
       title: "Inflow TXN Count",
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.inflow,
         "date",
-        "number_of_txns"
+        "number_of_txns",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(63, 205, 171, 1)",
       topColor: "rgba(63, 205, 171, 0.7)",
-      bottomColor: "rgba(63, 205, 171, 0.04)"
+      bottomColor: "rgba(63, 205, 171, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Avg. TXN Value (${tokenSymbol})`,
       title: `Outflow Avg. TXN Value (${tokenSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.outflow,
         "date",
-        "avg_txn_value"
+        "avg_txn_value",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(250, 78, 150, 1)",
       topColor: "rgba(250, 78, 150, 0.3)",
-      bottomColor: "rgba(250, 78, 150, 0.04)"
+      bottomColor: "rgba(250, 78, 150, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Avg. TXN Value (${tokenSymbol})`,
       title: `Inflow Avg. TXN Value (${tokenSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.inflow,
         "date",
-        "avg_txn_value"
+        "avg_txn_value",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(63, 205, 171, 1)",
       topColor: "rgba(63, 205, 171, 0.7)",
-      bottomColor: "rgba(63, 205, 171, 0.04)"
+      bottomColor: "rgba(63, 205, 171, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Avg. TXN Value (${USDSymbol})`,
       title: `Outflow Avg. TXN Value (${USDSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.outflow,
         "date",
-        "avg_txn_value_usd"
+        "avg_txn_value_usd",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(250, 78, 150, 1)",
       topColor: "rgba(250, 78, 150, 0.3)",
-      bottomColor: "rgba(250, 78, 150, 0.04)"
+      bottomColor: "rgba(250, 78, 150, 0.04)",
+      timeWindow
     },
     {
       dataPoint: `Avg. TXN Value (${USDSymbol})`,
       title: `Inflow Avg. TXN Value (${USDSymbol})`,
-      chartValues: toSingleValueChartDataForTimeWindow(
+      chartValues: toSingleValueChartData(
         response.inflow,
         "date",
-        "avg_txn_value_usd"
+        "avg_txn_value_usd",
+        timeWindow
       ),
       visible: false,
       solidColor: "rgba(63, 205, 171, 1)",
       topColor: "rgba(63, 205, 171, 0.7)",
-      bottomColor: "rgba(63, 205, 171, 0.04)"
+      bottomColor: "rgba(63, 205, 171, 0.04)",
+      timeWindow
     }
   ];
 
@@ -178,58 +182,66 @@ export const getExchangeDataSet = (response, token, timeWindow = "1d") => {
       {
         dataPoint: "Add. Count (entity)",
         title: "Outflow No. Sending Addresses",
-        chartValues: toSingleValueChartDataForTimeWindow(
+        chartValues: toSingleValueChartData(
           response.outflow,
           "date",
-          "number_of_entity_sending_addresses"
+          "number_of_entity_sending_addresses",
+          timeWindow
         ),
         visible: false,
         solidColor: "rgba(250, 78, 150, 1)",
         topColor: "rgba(250, 78, 150, 0.3)",
-        bottomColor: "rgba(250, 78, 150, 0.04)"
+        bottomColor: "rgba(250, 78, 150, 0.04)",
+        timeWindow
       },
       {
         dataPoint: "Add. Count (entity)",
         title: "Inflow No. Receiving Addresses",
-        chartValues: toSingleValueChartDataForTimeWindow(
+        chartValues: toSingleValueChartData(
           response.inflow,
           "date",
-          "number_of_entity_receiving_addresses"
+          "number_of_entity_receiving_addresses",
+          timeWindow
         ),
         visible: false,
         solidColor: "rgba(63, 205, 171, 1)",
         topColor: "rgba(63, 205, 171, 0.7)",
-        bottomColor: "rgba(63, 205, 171, 0.04)"
+        bottomColor: "rgba(63, 205, 171, 0.04)",
+        timeWindow
       },
       {
         dataPoint: "Add. Count (non-ent.)",
         title: "Outflow No. Receiving Addresses",
-        chartValues: toSingleValueChartDataForTimeWindow(
+        chartValues: toSingleValueChartData(
           response.outflow,
           "date",
-          "number_of_nonentity_receiving_addresses"
+          "number_of_nonentity_receiving_addresses",
+          timeWindow
         ),
         visible: false,
         solidColor: "rgba(250, 78, 150, 1)",
         topColor: "rgba(250, 78, 150, 0.3)",
-        bottomColor: "rgba(250, 78, 150, 0.04)"
+        bottomColor: "rgba(250, 78, 150, 0.04)",
+        timeWindow
       },
       {
         dataPoint: "Add. Count (non-ent.)",
         title: "Inflow No. Sending Addresses",
-        chartValues: toSingleValueChartDataForTimeWindow(
+        chartValues: toSingleValueChartData(
           response.inflow,
           "date",
-          "number_of_nonentity_sending_addresses"
+          "number_of_nonentity_sending_addresses",
+          timeWindow
         ),
         visible: false,
         solidColor: "rgba(63, 205, 171, 1)",
         topColor: "rgba(63, 205, 171, 0.7)",
-        bottomColor: "rgba(63, 205, 171, 0.04)"
+        bottomColor: "rgba(63, 205, 171, 0.04)",
+        timeWindow
       }
     ]);
-    return addTimeWindow(baseDataSetWithBTC, timeWindow);
+    return baseDataSetWithBTC;
   }
 
-  return addTimeWindow(baseDataSet, timeWindow);
+  return baseDataSet;
 };
