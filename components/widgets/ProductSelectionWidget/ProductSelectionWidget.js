0cbe97a3552da2f74fb9fb4bb989bd96bd92b11b
David Fava <dfava80@gmail.com>
2019-09-18 17:08:33 +0100

    Fix double quotes

--- components/widgets/ProductSelectionWidget/ProductSelectionWidget.js
+++ components/widgets/ProductSelectionWidget/ProductSelectionWidget.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-import { Product } from "./Product";
-import { PRODUCTS } from "./data/productsData";
+import { Product } from './Product';
+import { PRODUCTS } from './data/productsData';
 
 export const ProductSelectionWidget = () => {
   return (
