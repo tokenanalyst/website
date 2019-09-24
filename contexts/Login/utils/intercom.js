24ad77b3cbafe0645570b3a321e028a9617a3312
David Fava <dfava80@gmail.com>
2019-09-24 14:04:10 +0100

    Fix intercom

--- contexts/Login/utils/intercom.js
+++ contexts/Login/utils/intercom.js
@@ -1,17 +1,15 @@
 import { INTERCOM_APP_ID } from '../../../constants/intercom';
 
 const setUser = (name = '', email = '') => {
-  console.log('intercom loadded boot');
-  // window.Intercom('boot', {
-  //   app_id: INTERCOM_APP_ID,
-  //   name,
-  //   email,
-  //   created_at: new Date().getTime() / 1000,
-  // });
+  window.Intercom('boot', {
+    app_id: INTERCOM_APP_ID,
+    name,
+    email,
+    created_at: new Date().getTime() / 1000,
+  });
 };
 
 const removeUser = () => {
-  console.log('intercom loadded remove');
   window.Intercom('shutdown');
   window.Intercom('boot', {
     app_id: INTERCOM_APP_ID,
