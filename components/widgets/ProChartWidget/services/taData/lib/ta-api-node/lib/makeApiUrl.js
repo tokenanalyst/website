dc2917b55959207e3085ee91a7dd49007145834a
David Fava <dfava80@gmail.com>
2019-09-25 13:43:56 +0100

    Add relative URL to ta api client

--- components/widgets/ProChartWidget/services/taData/lib/ta-api-node/lib/makeApiUrl.js
+++ components/widgets/ProChartWidget/services/taData/lib/ta-api-node/lib/makeApiUrl.js
@@ -9,5 +9,5 @@ module.exports = (endpoint, restRootUrl, queryParams) => {
     separator = '?';
   }
 
-  return `${restRootUrl}/${endpoint}${separator}${makeQuery(queryParams)}`;
+  return `/api/${endpoint}${separator}${makeQuery(queryParams)}`;
 };
