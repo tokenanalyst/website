ff69cc8c7363fafb6954078d20a507074ef01014
jamesrford7 <jamesrford7@gmail.com>
2019-09-25 16:19:31 +0100

    Info

--- components/widgets/ProChartWidget/ProChartWidget.js
+++ components/widgets/ProChartWidget/ProChartWidget.js
@@ -11,6 +11,41 @@ import {
   EXCHANGE_DOLLARS,
 } from '../../../constants/exchanges';
 
+const TOOLTIP_TEXT = (
+  <>
+    <div>Below is the legend for each of the charts displayed</div>
+    <div>
+      All units are displayed in terms of the selected asset except for the
+      Price which is in USD or USDT
+    </div>
+    <br />
+    <div>
+      1: Price of the selected asset on the selected exchange in either USD or
+      USDT
+    </div>
+    <br />
+    <div>
+      2: Trading volume of the pair shown above on the selected exchange.
+      Expressed in terms of the selected asset
+    </div>
+    <br />
+    <div>
+      3: Inflows and outflows of the selected asset into and out of the selected
+      exchange on the blockchain
+    </div>
+    <br />
+    <div>
+      4: "Net" flows of the asset into the exchange on the blockchain. This is
+      the sum of inflows minus the outflows
+    </div>
+    <div>
+      Negative net flows mean more funds left the exchange than came in during
+      the time interval and vice versa for positive net flows
+    </div>
+  </>
+);
+// ('Below is the legend for each of the charts displayed. \n(All units are displayed in terms of the selected asset except for the Price which is in USD or USDT). 1: Price of the selected asset on the selected exchange in either USD or USDT. 2: Trading volume of the pair shown above on the selected exchange. Expressed in terms of the selected asset. 3: Inflows and outflows of the selected asset into and out of the selected exchange on the blockchain. 4: "Net" flows of the asset into the exchange on the blockchain. This is the sum of inflows minus the outflows. Negative net flows mean more funds left the exchange than came in during the time interval and vice versa for positive net flows.');
+
 const STUDIES = {
   FLOWS: 'Flows',
   NET_FLOWS: 'NetFlows',
@@ -82,7 +117,7 @@ export const ProChartWidget = ({
             <div className="control">
               <SimpleToolTip
                 dataFor={'header-tooltip'}
-                toolTip={'Blah blah'}
+                toolTip={TOOLTIP_TEXT}
                 type="dark"
                 effect="solid"
               >
