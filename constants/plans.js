87d1699bd6423676886b9034a961ccbb646c783d
jamesrford7 <jamesrford7@gmail.com>
2019-10-01 18:39:36 +0100

    Comment out platform package for now

--- constants/plans.js
+++ constants/plans.js
@@ -1,6 +1,6 @@
 export const PLAN_NAMES = {
   FREE: 'Free',
-  PLATFORM: 'Platform',
+  // PLATFORM: 'Platform',
   PRO: 'Pro',
   ENTERPRISE: 'Enterprise',
 };
@@ -15,17 +15,17 @@ export const PLANS = {
     ],
     buttonText: 'Get',
   },
-  PLATFORM: {
-    name: PLAN_NAMES.PLATFORM,
-    price: '$50',
-    features: [
-      'Exchange Flows',
-      'Full Historical Data',
-      'Web Platform Access Only',
-    ],
-    buttonText: 'Purchase',
-    stripePlan: 'plan_FuFH6oh6AGeUl0',
-  },
+  // PLATFORM: {
+  //   name: PLAN_NAMES.PLATFORM,
+  //   price: '$50',
+  //   features: [
+  //     'Exchange Flows',
+  //     'Full Historical Data',
+  //     'Web Platform Access Only',
+  //   ],
+  //   buttonText: 'Purchase',
+  //   stripePlan: 'plan_FuFH6oh6AGeUl0',
+  // },
   PRO: {
     name: PLAN_NAMES.PRO,
     price: '$799',
@@ -56,17 +56,17 @@ export const TEST_PLANS = {
     features: ['Fundamental Analytics', '90 Day Data', 'CSV & API'],
     buttonText: 'Get',
   },
-  PLATFORM: {
-    name: PLAN_NAMES.PLATFORM,
-    price: '$50',
-    features: [
-      'Exchange Flows',
-      'Full Historical Data',
-      'Web Platform Access Only',
-    ],
-    buttonText: 'Purchase',
-    stripePlan: 'plan_FuFaK78eUjiZ9L',
-  },
+  // PLATFORM: {
+  //   name: PLAN_NAMES.PLATFORM,
+  //   price: '$50',
+  //   features: [
+  //     'Exchange Flows',
+  //     'Full Historical Data',
+  //     'Web Platform Access Only',
+  //   ],
+  //   buttonText: 'Purchase',
+  //   stripePlan: 'plan_FuFaK78eUjiZ9L',
+  // },
   HOBBYIST: {
     name: PLAN_NAMES.HOBBYIST,
     price: '$99',
