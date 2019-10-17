717fb953131181836f7d9941764b63fc3319c4a8
jamesrford7 <jamesrford7@gmail.com>
2019-10-17 13:49:20 +0100

    More metrics

--- constants/tokens.js
+++ constants/tokens.js
@@ -115,12 +115,20 @@ export const METRICS = {
       apiValue: 'token_fees_window_historical',
       values: [
         {
-          name: 'Real',
+          name: 'Total (Real)',
           apiValue: 'total_fee',
         },
         {
-          name: 'USD',
+          name: 'Total (USD)',
           apiValue: 'total_fee_usd',
+        },
+        {
+          name: 'Average (Real)',
+          apiValue: 'avg_fee',
+        },
+        {
+          name: 'Average (USD)',
+          apiValue: 'avg_fee_usd',
         },
         {
           name: 'Averge Size (Bytes)',
@@ -128,7 +136,141 @@ export const METRICS = {
         },
         {
           name: 'Averge Satoshis per Byte',
-          apiValue: 'total_fee',
+          apiValue: 'avg_satoshis_per_byte',
+        },
+      ],
+    },
+    {
+      category: 'UTXO Age',
+      apiValue: 'token_utxo_age_window_historical',
+      values: [
+        {
+          name: '<1d',
+          apiValue: '<1d',
+        },
+        {
+          name: '1-3m',
+          apiValue: '1-3m',
+        },
+        {
+          name: '3-6m',
+          apiValue: '3-6m',
+        },
+        {
+          name: '6-12m',
+          apiValue: '6-12m',
+        },
+        {
+          name: '12-18m',
+          apiValue: '12-18m',
+        },
+        {
+          name: '18-24m',
+          apiValue: '18-24m',
+        },
+        {
+          name: '1d-1w',
+          apiValue: '1d-1w',
+        },
+        {
+          name: '1w-1m',
+          apiValue: '1w-1m',
+        },
+        {
+          name: '2-3y',
+          apiValue: '2-3y',
+        },
+        {
+          name: '3-5y',
+          apiValue: '3-5y',
+        },
+        {
+          name: '5-10y',
+          apiValue: '5-10y',
+        },
+        {
+          name: '>10y',
+          apiValue: '>10y',
+        },
+      ],
+    },
+  ],
+  [NATIVE_TOKENS.ETH]: [
+    {
+      category: 'Volume',
+      apiValue: 'token_volume_window_historical',
+      values: [
+        {
+          name: 'Internal (Real)',
+          apiValue: 'volume_internal',
+        },
+        {
+          name: 'Internal (USD)',
+          apiValue: 'volume_internal_usd',
+        },
+        {
+          name: 'External (Real)',
+          apiValue: 'volume_external',
+        },
+        {
+          name: 'External (USD)',
+          apiValue: 'volume_external_usd',
+        },
+        {
+          name: 'Gross (Real)',
+          apiValue: 'volume_gross',
+        },
+        {
+          name: 'Gross (USD)',
+          apiValue: 'volume_gross_usd',
+        },
+      ],
+    },
+    {
+      category: 'Transactions',
+      apiValue: 'token_count_window_historical',
+      values: [
+        {
+          name: 'Number',
+          apiValue: 'number_of_txns',
+        },
+      ],
+    },
+    {
+      category: 'Addresses',
+      apiValue: 'token_active_address_window_historical',
+      values: [
+        {
+          name: 'Senders',
+          apiValue: 'active_senders',
+        },
+        {
+          name: 'Recipients',
+          apiValue: 'active_recipients',
+        },
+      ],
+    },
+    {
+      category: 'Supply',
+      apiValue: 'token_supply_window_historical',
+      values: [
+        {
+          name: 'Amount',
+          apiValue: 'supply',
+        },
+      ],
+    },
+    {
+      category: 'NVT',
+      apiValue: 'token_nvt_window_historical',
+      values: [
+        {
+          name: 'NVT',
+          apiValue: 'nvt',
+        },
+        {
+          name: 'Market Cap',
+          apiValue: 'marketcap_usd',
         },
       ],
     },
