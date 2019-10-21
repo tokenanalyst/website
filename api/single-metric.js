bb61f7bfe5d541945bc623a97dd40de545a5abec
jamesrford7 <jamesrford7@gmail.com>
2019-10-21 16:28:31 +0100

    Auth for tier 1+ users

--- api/single-metric.js
+++ api/single-metric.js
@@ -1,5 +1,8 @@
 const url = require('url');
 const axios = require('axios');
+const getUserAuth = require('./auth/getUserAuth');
+const makeUnixtimeLimit = require('./utils/makeUnixtimeLimit');
+const filterSeriesByTime = require('./utils/filterSeriesByTime');
 
 module.exports = async (req, res) => {
   const urlParts = url.parse(req.url, true);
@@ -7,9 +10,16 @@ module.exports = async (req, res) => {
     query: { metric, token, window, from_date, to_date },
   } = urlParts;
 
+  const { userData } = await getUserAuth(req.cookies.apiKey);
+
+  const tierTimeLimit = makeUnixtimeLimit(
+    window,
+    userData.tier.timeLimits[window]
+  );
+
   const result = await axios.get(
     `https://api.tokenanalyst.io/analytics/private/v1/${metric}/last?token=${token}&window=${window}&from_date=${from_date}&to_date=${to_date}&key=${process.env.API_KEY}&format=json`
   );
 
-  res.send(result.data);
+  res.send(filterSeriesByTime(result.data, tierTimeLimit));
 };
