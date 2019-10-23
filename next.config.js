18eae5b68150f69cb2709418cdeb988ec96a7fd4
David Fava <dfava80@gmail.com>
2019-10-23 14:29:32 +0100

    Fix next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const withSourceMaps = require('@zeit/next-source-maps')({});
 
 // Generates a common build id to allow matching of generated source maps with deployed build in production
 const generateBuildId = async () => {
-  return process.env.SENTRY_RELEASE; //
+  if (process.env.SENTRY_RELEASE) {
+    return process.env.SENTRY_RELEASE;
+  }
+
+  return null;
 };
 
 const NEXT_CONFIG = {
