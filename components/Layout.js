e1499ef956271b0f8b5ff6d2e59b029ba2d54b7d
jamesrford7 <jamesrford7@gmail.com>
2019-10-23 13:58:45 +0100

    Missed a metrics - insights

--- components/Layout.js
+++ components/Layout.js
@@ -19,7 +19,7 @@ const STRUCTURED_DATA = JSON.stringify({
   url: 'https://www.tokenanalyst.io',
 });
 
-const WITHOUT_FOOTER = ['/exchange/[token]/[exchange]', '/metrics'];
+const WITHOUT_FOOTER = ['/exchange/[token]/[exchange]', '/insights'];
 
 export const Layout = ({ children }) => {
   const router = useRouter();
