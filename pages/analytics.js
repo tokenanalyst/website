77a5ff27dd9bba705ab0f6843e34d7d3049a4fd3
jamesrford7 <jamesrford7@gmail.com>
2019-11-06 14:15:29 +0000

    Finish up

--- pages/analytics.js
+++ pages/analytics.js
@@ -70,7 +70,9 @@ const Analytics = () => {
 
   useEffect(() => {
     const getCharts = async () => {
-      const res = await axios.get('http://localhost:8050/chart-metadata');
+      const res = await axios.get(
+        'https://ta-plotly-dash.herokuapp.com/chart-metadata'
+      );
       setCharts(res.data.items);
       setCategories(res.data.categories);
       setSelectedCategory(res.data.categories[0]);
