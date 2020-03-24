5b1af676518ca596dd3fc5b70da7398843fdf29b
David Fava <dfava80@gmail.com>
2020-03-24 09:48:45 +0000

    Fix stats page layout

--- components/atomic/pages/Metrics/Metrics.js
+++ components/atomic/pages/Metrics/Metrics.js
@@ -1,12 +1,10 @@
 import React, { useState, useRef } from 'react';
-import { Card } from '@blueprintjs/core';
-import ReactGA from 'react-ga';
 import moment from 'moment';
 
-import { TokenSelect } from '../../organism/TokenSelect/TokenSelect';
 import { tokensDb } from '../../../../services/tokensDb';
 import { TOKEN_NAMES } from '../../../../constants/token-names';
 import { ProChartContainer } from '../../organism/ProChartContainer';
+import { KaikoLogo } from '../../atoms/KaikoLogo';
 import {
   NATIVE_TOKENS,
   STABLE_TOKENS,
@@ -18,8 +16,7 @@ import {
   BINANCE,
 } from '../../../../constants/exchanges';
 import { SPOT } from '../../../../constants/instruments';
-import { MetricsList } from './MetricsList';
-import { EntityLogo } from '../../molecules/EntityLogo';
+import { LeftSidePanelMetrics } from '../../organism/LeftSidePanelMetrics';
 
 const TV_INITIAL_DATA_RANGE = 90; // 90 days
 
@@ -93,106 +90,97 @@ export const MetricsPage = () => {
   return (
     <>
       <div className="container">
-        <div className="lhs">
-          <div className="title">
-            <EntityLogo
-              tokenSymbol={selectedToken}
-              entityName={TOKEN_NAMES[selectedToken]}
-            />
-          </div>
-          <Card>
-            <TokenSelect
-              items={tokensList}
-              groups={['Native coins', 'Stablecoins', 'ERC20 tokens']}
+        <div className="left-panel">
+          <div className="controls-box">
+            <LeftSidePanelMetrics
               selectedToken={selectedToken}
-              onItemSelect={newToken => {
-                ReactGA.event({
-                  category: 'User',
-                  action: `Metrics Page change token ${newToken}`,
-                  label: `Metrics Page`,
-                });
-                setSelectedToken(newToken);
-              }}
-            />
-          </Card>
-          <div className="metrics-list">
-            <MetricsList
-              token={selectedToken}
+              tokenName={TOKEN_NAMES[selectedToken]}
+              onChangeToken={setSelectedToken}
+              tokensList={tokensList}
               selectedIndicator={selectedIndicator}
               setSelectedIndicator={setSelectedIndicator}
             />
           </div>
         </div>
-        <div className="rhs">
-          <ProChartContainer
-            timeFrame="3D"
-            interval="60"
-            TVSymbols={[baseToken, quoteToken]}
-            TVOptions={TVOptions}
-            TASymbol={selectedToken}
-            exchangeName={exchangeName}
-            instrumentClass={SPOT}
-            isIntraDay={selectedIndicator.isIntraDay}
-            onChartRenderCb={async tvWidget => {
-              tvInstance.current = tvWidget;
-              const now = moment().unix();
-              const ninetyDaysAgo = moment()
-                .subtract(TV_INITIAL_DATA_RANGE, 'days')
-                .unix();
-              await tvInstance.current.chart().setVisibleRange({
-                from: ninetyDaysAgo,
-                to: now,
-              });
-              studies.current.flows.entityId = tvInstance.current
-                .chart()
-                .createStudy(selectedIndicator.name, false, true);
-            }}
-          />
+        <div className="right-panel">
+          <div className="pro-chart">
+            <ProChartContainer
+              timeFrame="3D"
+              interval="60"
+              TVSymbols={[baseToken, quoteToken]}
+              TVOptions={TVOptions}
+              TASymbol={selectedToken}
+              exchangeName={exchangeName}
+              instrumentClass={SPOT}
+              isIntraDay={selectedIndicator.isIntraDay}
+              onChartRenderCb={async tvWidget => {
+                tvInstance.current = tvWidget;
+                const now = moment().unix();
+                const ninetyDaysAgo = moment()
+                  .subtract(TV_INITIAL_DATA_RANGE, 'days')
+                  .unix();
+                await tvInstance.current.chart().setVisibleRange({
+                  from: ninetyDaysAgo,
+                  to: now,
+                });
+                studies.current.flows.entityId = tvInstance.current
+                  .chart()
+                  .createStudy(selectedIndicator.name, false, true);
+              }}
+            />
+          </div>
+          <div className="kaiko">
+            <KaikoLogo />
+          </div>
         </div>
       </div>
       <style jsx>
         {`
           .container {
             display: flex;
+            flex-direction: row;
+            justify-content: space-between;
           }
-          .lhs {
-            padding-right: 10px;
-            width: 250px;
+          .controls-box {
           }
-          .rhs {
-            width: 85%;
+          .left-panel {
+            width: 300px;
           }
-          .title {
-            padding-top: 10px;
+          .right-panel {
+            width: 100%;
+            margin-left: 10px;
+          }
+          .cat-link {
             padding-bottom: 10px;
           }
-          .title-image {
-            width: 60px;
+          .pro-chart {
+            width: 100%;
           }
-          .title-name {
-            font-size: 20px;
-            font-weight: bold;
-            padding-left: 10px;
+          .kaiko {
+            padding-top: 5px;
+            padding-bottom: 5px;
+            text-align: right;
           }
-          .metrics-list {
-            padding-top: 10px;
+
+          @media (min-width: 768px) and (max-width: 1440px) {
+            .controls-box {
+            }
+            .pro-chart {
+            }
           }
           @media (min-width: 320px) and (max-width: 767px) {
             .container {
-              flex-direction: column-reverse;
+              flex-direction: column;
             }
-            .lhs {
-              width: 100%;
-              margin-bottom: 150px;
-            }
-            .rhs {
+            .left-panel {
               width: 100%;
             }
-            .title-image {
-              display: none;
+            .pro-chart {
+              padding-top: 5px;
+              width: 100%;
             }
-            .title-name {
-              display: none;
+            .controls-box {
+              width: 100%;
             }
           }
         `}
