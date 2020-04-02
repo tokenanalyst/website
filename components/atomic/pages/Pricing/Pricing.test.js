70f1ee816f0747615889bba4bc68f7e90f0fbf2e
David Fava <dfava80@gmail.com>
2020-04-02 12:14:54 +0100

    Add PagerDuty alert for stripe

---
+++ components/atomic/pages/Pricing/Pricing.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { redirectToStripe } from '../../../../utils';
+import { PricingPage } from './Pricing';
+
+jest.mock('../../../../utils');
+redirectToStripe.mockReturnValue(() => {});
+
+describe('Pricing component', () => {
+  it('simulates click events', () => {
+    const wrapper = mount(<PricingPage />);
+    wrapper.find('button').forEach(button => {
+      console.log(button.text());
+      if (button.text() === 'Buy Plan') {
+        button.simulate('click');
+        expect(redirectToStripe).toHaveBeenCalledWith(
+          'plan_FZwuSdyp2hRm98',
+          'Professional'
+        );
+      }
+    });
+  });
+});
---
+++ constants/pagerDuty.js
@@ -0,0 +1,9 @@
+export const PD = {
+  serviceKey: 'e8048d9c3211488caa16e1bd2cad3e03',
+  severity: {
+    2: 'critical',
+    3: 'error',
+    6: 'info',
+    4: 'warning',
+  },
+};
